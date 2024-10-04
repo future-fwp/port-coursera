1a2028eaeb608cbd8ca592a882daf6e172e028a3
future-fwp <6532161321@student.chula.ac.th>
2024-10-04 22:09:10 +0700

    portfolio-react

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,8 +4,8 @@ const Profile = () => {
 	return (
 		<section className="text-center mb-10 h-[calc(100vh-40px)] flex justify-center items-center flex-col">
 			<div className="bg-gray-500 rounded-full w-24 h-24 mx-auto mb-4"></div>
-			<h1 className="text-2xl lg:text-3xl font-bold">Senior Frontend Engineer</h1>
-			<p className="text-lg mt-2">Specialized in React</p>
+			<h1 className="text-2xl lg:text-3xl font-bold">junior Frontend Engineer</h1>
+			<p className="text-lg mt-2">Intermediate in React</p>
 		</section>
 	);
 };
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,26 @@
 import React from "react";
-
+const ProjectName = [
+	{
+		name: "React Project Title1",
+		description: "React project description1",
+		image: "https://via.placeholder.com/150",
+	},
+	{
+		name: "React Project Title2",
+		description: "React project description2",
+		image: "https://via.placeholder.com/150",
+	},
+	{
+		name: "React Project Title3",
+		description: "React project description3",
+		image: "https://via.placeholder.com/150",
+	},
+	{
+		name: "React Project Title4",
+		description: "React project description4",
+		image: "https://via.placeholder.com/150",
+	},
+];
 const Project = () => {
 	return (
 		<section
@@ -9,16 +30,21 @@ const Project = () => {
 			<h2 className="text-2xl font-bold mb-6 text-center">Feature Project</h2>
 			<div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
 				{/* Project Cards */}
-				{Array(4)
-					.fill(0)
-					.map((_, index) => (
+				{ProjectName &&
+					ProjectName.map((each, index) => (
 						<div
 							key={index}
 							className="bg-gray-800 p-4 rounded-md shadow-md"
 						>
-							<div className="bg-gray-400 h-32 lg:h-40 mb-4"></div>
-							<h3 className="text-lg lg:text-xl font-semibold mb-2">React Project Title</h3>
-							<p className="text-sm mb-4">React project description</p>
+							<div className="h-32 lg:h-40 mb-4">
+								<img
+									className="object-cover w-full h-full rounded-md"
+									src={each.image}
+									alt={each.name}
+								/>
+							</div>
+							<h3 className="text-lg lg:text-xl font-semibold mb-2">{each.name}</h3>
+							<p className="text-sm mb-4">{each.description}</p>
 							<a
 								href="#"
 								className="text-blue-400 hover:underline"
